75aaf46a5a43659e9ab98ed8b9d75ec1fcf30ebc
rakesh2971 <134310724+rakesh2971@users.noreply.github.com>
2024-11-15 19:09:10 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -7,3 +7,48 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBvBheGoO6EGNOoBquiIiAKIneae6dQZYU",
+  authDomain: "gen-ai-web.firebaseapp.com",
+  databaseURL: "https://gen-ai-web-default-rtdb.firebaseio.com",
+  projectId: "gen-ai-web",
+  storageBucket: "gen-ai-web.firebasestorage.app",
+  messagingSenderId: "266109203719",
+  appId: "1:266109203719:web:ee2a93477b69ac570c81a2",
+  measurementId: "G-HJE5NR631L"
+  };
+
+  // initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+
+var LoginInfoDB = firebase.database().ref("LoginInfo");
+
+document.getElementById('LoginForm').addEventListener('submit',submitForm);
+function submitForm(e){
+  e.preventDefault();
+
+  var Name = getElementVal("Name");
+  var Email = getElementVal("Email");
+  var Password= getElementVal("Password");
+
+  savedMessages(Name, Email, Password);
+
+  //console.log(Name, Email, Password);
+}
+
+const savedMessages = (Name, Email, Password) => {
+  var newLoginInfo = LoginInfoDB.push();
+
+  newLoginInfo.set({
+    Name: Name,
+    Email:Email,
+    Password: Password,
+  });
+};
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
